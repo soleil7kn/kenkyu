5d9823835d487aec32f017c4ee3e46ef802e03ce
soleil7kn <kota.nakai.8v@stu.hosei.ac.jp>
2026-06-21 23:04:34 +0900

    SkipTimeformerをいったんもとに戻した

--- iTransformer/model/SkipTimeformer.py
+++ iTransformer/model/SkipTimeformer.py
@@ -71,6 +71,11 @@ class Model(nn.Module):
             configs.dropout
         )
 
+        # Multi Skip
+        self.multi_skip = MultiSkipEmbedding(
+            skip_rates=[2, 4]
+        )
+
         # Transformer Encoder
         self.encoder = Encoder(
             [
@@ -96,22 +101,8 @@ class Model(nn.Module):
         )
 
         # Prediction Head
-        self.skip_rates = [2, 4]
-
-        self.num_skip = sum(self.skip_rates)
-
-        self.max_len = max(
-            (configs.seq_len + s - 1) // s
-            for s in self.skip_rates
-        )
-
-        #self.skip_logits = nn.Parameter(
-        #    torch.zeros(self.num_skip)
-        #)
-
-        self.multi_skip = MultiSkipEmbedding(
-            skip_rates=self.skip_rates
-        )
+        self.max_len = (configs.seq_len + 1) // 2
+        self.num_skip = 6
 
         self.head = nn.Linear(
             self.num_skip * self.max_len * configs.d_model,
@@ -144,7 +135,7 @@ class Model(nn.Module):
             enc_out
         )
 
-        # print("skip_tokens :", skip_tokens.shape)
+        print("skip_tokens :", skip_tokens.shape)
 
         # [B, M, L, D]
         B, M, L, D = skip_tokens.shape
@@ -176,16 +167,6 @@ class Model(nn.Module):
             L,
             D
         )
-        """
-        # skip重みを計算
-        weights = torch.softmax(
-            self.skip_logits,
-            dim=0
-        ).view(1, M, 1, 1)
-
-        # skipごとに重み付け
-        enc_out = enc_out * weights
-        """
 
         enc_out = enc_out.reshape(
             B,
